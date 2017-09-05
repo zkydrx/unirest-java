07dbe840ccc0072a1914e65bd97fb235fb987515
ZKYAbbot <1964483239@qq.com>
2017-09-06 01:42:49 +0800

    修改格式

--- src/main/java/com/mashape/unirest/http/utils/GoogleShortUtil.java
+++ src/main/java/com/mashape/unirest/http/utils/GoogleShortUtil.java
@@ -15,27 +15,27 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 	*/
 public class GoogleShortUtil
 {
-	/**
-		* 基于Google short url 实现短链接
-		* 优点，速度快，功能多。
-		*
-		* @param longUrl
-		* @return
-		*/
-	public static String createShortUrl(String longUrl) throws UnirestException
-	{
-						HttpResponse<JsonNode> body = Unirest.post("https://www.googleapis" + "" + ".com/urlshortener/v1/url")
-																																							.header("Content-Type", "application/json")
-																																							.queryString("key", "AIzaSyAdkwBkPoXODWAH3NPf8ZI8lMzR9TlyDRo")
-																																							.body("{\"longUrl\":\"" + longUrl + "\"}")
-																																							.asJson();
+					/**
+						* 基于Google short url 实现短链接
+						* 优点，速度快，功能多。
+						*
+						* @param longUrl
+						* @return
+						*/
+					public static String createShortUrl(String longUrl) throws UnirestException
+					{
+										HttpResponse<JsonNode> body = Unirest.post("https://www.googleapis" + "" + ".com/urlshortener/v1/url")
+																																											.header("Content-Type", "application/json")
+																																											.queryString("key", "AIzaSyAdkwBkPoXODWAH3NPf8ZI8lMzR9TlyDRo")
+																																											.body("{\"longUrl\":\"" + longUrl + "\"}")
+																																											.asJson();
 
-						String shortUrl = body.getBody().getObject().getString("id");
-						return shortUrl;
-	}
+										String shortUrl = body.getBody().getObject().getString("id");
+										return shortUrl;
+					}
 
-	public static void main(String[] args) throws UnirestException
-	{
-						String shortUrl = createShortUrl("https://github.com/zkydrx/unirest-java");
-	}
+					public static void main(String[] args) throws UnirestException
+					{
+										String shortUrl = createShortUrl("https://github.com/zkydrx/unirest-java");
+					}
 }
